2cf5aa8e2def53311d79294072c6715f36a9c1ef
Hossein <hpourjafarabadi.dev@gmail.com>
2026-07-01 15:28:54 +0330

    User model created

--- app/main.py
+++ app/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from .db.database import engine
 from .db.base import Base
 
+from app.models.user import User
+
 app = FastAPI()
 
-Base.metadata.create_all(bind = engine)
+Base.metadata.create_all(bind=engine)
---
+++ app/models/user.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, DateTime, Enum
+from datetime import datetime, timezone
+import uuid
+import enum
+from app.db.base import Base
+
+class UserRole(enum.Enum):
+    admin = "admin"
+    user = "user"
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(String, primary_key=True, index=True, default=lambda: uuid.uuid4().hex)
+    name = Column(String(30), nullable=False)
+    phone = Column(String(12), nullable=False, unique=True)
+    password = Column(String(255), nullable=False)
+    role = Column(Enum(UserRole), default=UserRole.user, nullable=False)
+    created_at = Column(DateTime, default=datetime.now(timezone.utc))
